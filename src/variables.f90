bdc7d59fd33dc6193e674f107a17c893127ec53b
VPezlar <pezlar.vojtech@gmail.co,m>
2025-11-02 16:52:18 +0200

    new

--- src/variables.f90
+++ src/variables.f90
@@ -6,5 +6,6 @@ MODULE variables
 
     INTEGER(ik) :: N_HEADER_grid, N_HEADER_var
     CHARACTER(len=256) :: file_grid, file_var, output_file, flow_format, COMMAND_RUN
+    REAL(rk) :: dist_mag
 
 END MODULE variables
