cbe1ae14dab86fc420948af34f87d252f7cfb6e8
VPezlar <pezlar.vojtech@gmail.co,m>
2025-11-02 17:21:05 +0200

    new

--- src/main.f90
+++ src/main.f90
@@ -10,7 +10,7 @@ PROGRAM main
     IMPLICIT NONE
 
     REAL(rk), DIMENSION(:), ALLOCATABLE :: rho_in, p_in, T_in, U_in, V_in, W_in, Xgrid, Ygrid, Zgrid, pert_0
-    INTEGER(ik) :: data_count
+    INTEGER(ik) :: data_count, unit_num
     INTEGER(ik) :: error_status, STATUS_CODE
 
     ! Read configuration
